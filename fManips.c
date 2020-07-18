75d93d0c42425f68c6b7250b2f541139a6bcec41
doughnuty <54584786+doughnuty@users.noreply.github.com>
2020-07-18 20:21:40 +0600

    Update fManips.c

--- fManips.c
+++ fManips.c
@@ -1,45 +1,143 @@
-#include "my_tar.h"
+#include <unistd.h>
+#include "internalFuncts.c"
 
-struct headerInfo {
+int displayheader(struct posix_header *header)
+{
+    printf("======= EXTRACTING THE FILE =======\n");
+    printf("GETTING THE HEADER...\n");
+    printf("Filename is %s\n", header->name);
+    printf("%s\n", header->size);
+    printf( "===================================\n" );
 
-};
-int getContents(char* filename, int fd); // Write file contents of size 512 either to a new or a tar file, fd of which is passed
-
-char* getHeader(struct headerInfo hi); // Create the file header based on the information provided
-
-struct headerInfo reverseHeader(int archfd); // Get the file info from its header
-
-int printFiles(char** filenames); // Print the filenames if -t provided
-
-int fillMissing(int fd_arch); // Adds bzero to archive
-
-int error(int errnum); // Print error message depending on the error case
-
-int create(int fd_arch, char** files) // fill tar file with contents
+    return 0;
+}
+int append()
 {
     int chk = 0;
-    while(files)
-    {
-        char* header = getHeader(files[i]);
-        write(fd_arch, header, 512);
-        chk += getContents(files[i], fd_arch);
-        if(chk > 0) break;
-    }
-    chk += fillMissing(fd_arch);
+    return chk;
 }
 
-int extract(int fd_arch, char** files) // extract the contents to the new files
+int appendNew()
 {
     int chk = 0;
-    while(tar)
+    return chk;
+}
+int create(int fd_arch, char** files, int fcount) // fill tar file with contents
+{
+    int chk = 0;
+    printf("fcount = %d", fcount);
+    struct posix_header *header;
+    for(int fileNum = 0; fileNum != fcount; fileNum ++)
     {
-        struct headerInfo header = reverseHeader(fd_arch);
-        if (header.type == REGULAR)
-            chk += createReg(header);
-        else if (header.type == DIRECTORY)
-            Cchk += createDir(header);
-        else return 1;
+        header = getHeaderInfo( files[ fileNum ] );
+        if( header != NULL ) {
+            write(fd_arch, header, 512);
+            if (header->typeflag != DIRTYPE)
+                writeFileContent( fd_arch, files[ fileNum ] );
+            else 
+            {
+                chk -= recursiveTar(header->name, fd_arch);
+            }
+            free( header );
+        } else {
+            chk ++;
+            printf("tar: Error archiving %s\n", files[fileNum]);
+        }
     }
+
+    fillMissing( fd_arch );
+    return chk;
 }
 
-int list(int fd_arch, char** files); // list the filenames
+int extract(int fd_arch, char *archname, int fcount, char** files) // extract the contents to the new files
+{
+    long int i = 0;
+    long int size = 0;
+    int chk = 0;
+    int fchk = 0;
+    //int size = 0;
+    long int archsize = getSize(archname);
+    printf("tar file size is %ld\n", archsize);
+    while(i < archsize)
+    {
+        // if (i > 0)  
+        //     i += 512 - (i % 512); 
+        headerInfo *header = reverseHeader(fd_arch, i);
+        i += 512;
+        displayheader(header);
+        if((fcount > 0 && cmpName(files[fchk], header->name) == 0) || fcount == 0)
+        {
+            if (header != NULL && getLength(header->name) > 0) 
+            {
+                if (header->typeflag != DIRTYPE)
+                {
+                    createReg(header, fd_arch);
+                    long int size = getBlocksSize(reverseConvert(header->size, 8)); 
+                    i += size;
+                }
+                else
+                {
+                    chk -= createDir(header);
+                    printf("This is a dir with the name %s\n", header->name);
+                }
+                free(header);
+            }
+            else
+            {
+                chk++;
+                free(header);
+                return chk;
+            }
+        }
+
+        printf("Stopped at %ld byte. Size is %ld\n", i, size);
+        if (i < 0 && size < 0)
+            return 2;
+    }
+
+    return chk;
+}
+
+int list(int archfd, int fcount, char** filenames) // list the filenames
+{
+    int chk = 0;
+    int i = 0;
+    char* filename = malloc(100);
+    while(filename != NULL)
+    {
+        headerInfo *header = reverseHeader(archfd, i);
+        filename = header->name;
+        if (fcount == 0 || (fcount > 0 && cmpName(filenames[i], filename) == 0))
+        {
+            if (getLength(filename) <= 0)
+                break;
+            long int size = getBlocksSize(reverseConvert(header->size, 8)); 
+            i += 512 + size;
+            printf("%s\n", filename);
+        }
+    }
+    free(filename);
+    return chk;
+}
+
+int fManip(struct Options opts)
+{
+    switch (opts.mod)
+    {
+        case 'r':
+            append();
+            break;
+        case 'u':
+            appendNew();
+            break;
+        case 'c':
+            create(opts.archfd, opts.files, opts.fcount);
+            break;
+        case 'x':
+            extract(opts.archfd, opts.archname, opts.fcount, opts.files);
+            break;
+        case 't':
+            list(opts.archfd, opts.fcount, opts.files);
+    }
+    return 0;
+}
